55be0fb816cf34afba9b6c9060ff1f98bb58d438
yamato <titechymt@gmail.com>
2016-04-22 16:04:38 +0900

    String resourselization

--- app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
     private TextView outputView;
     private EditText inputName;
@@ -21,17 +21,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outputView = (TextView)findViewById(R.id.output_view);
         inputName = (EditText)findViewById(R.id.input_name);
         okButton = (Button)findViewById(R.id.ok_button);
-        okButton.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.ok_button:
+        /*okButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
                 String name = inputName.getText().toString();
                 if(name != null && name.length() > 0)
                     outputView.setText("Hello, " + name + "\nNice to see you!");
-                break;
-        }
+            }
+        });*/
+    }
+
+    public void greet(View v){
+        String name = inputName.getText().toString();
+        if(name.length() > 0)
+            outputView.setText("Hello, " + name + ".\nNice to see you!");
     }
 }
